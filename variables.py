7832281c3ce39d16f0c856797d365f7e61f39a0e
Ashraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2023-10-14 23:16:24 +0300

    Add files via upload
    learn variables in python

---
+++ variables.py
@@ -0,0 +1,14 @@
+name = 'ashraf mohamed'
+age = '21'
+student = 'delta'
+
+print('hello ' + name + ' my age is ' + age)
+print('work at ' + student +  ' my name ' + name + ' and my age is ' + age)
+
+
+
+name = 'ali'
+age = '33'
+
+print('hello ' + name + ' my age is ' + age)
+print('work at ' + student +  ' my name ' + name + ' and my age is ' + age)
