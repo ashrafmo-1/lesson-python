3cd31158202d032800c0ebcf98ab0026349c8104
Ashraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2023-10-15 17:14:09 +0300

    Add files via upload
    add built in functions for string and numbers

---
+++ builtIn-Function.py
@@ -0,0 +1,26 @@
+# all built-in functions used in this module
+# max => get height number
+# min => get small number
+# round => Rounding to the nearest number
+# floor => Rounding to the smallest number "main import from math => from math import floor"
+# ceil => Rounding to the largest number "main import from math => from math import floor"
+# sqrt => Quadratic Islands || الجزر التربيعي
+# pow => for example pow(10, 4) => 10 * 10 * 10 * 10
+
+print("#### starting built-in functions for numbers ####")
+print(max(10, 99))
+print(min(10, 99))
+print(pow(10, 4))
+print(round(10.6))
+
+from math import floor
+print(floor(10.6))
+
+from math import ceil
+print(ceil(10.2))
+
+from math import sqrt
+print(sqrt(100))
+
+
+print("#### starting built-in functions for strings ####")
