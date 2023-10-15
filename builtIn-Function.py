a82b301b344425a6848c99002fec61a83d78c6b4
Ashraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2023-10-15 22:54:03 +0300

    Add files via upload
    add built-in functions for strings

--- builtIn-Function.py
+++ builtIn-Function.py
@@ -1,4 +1,6 @@
 # all built-in functions used in this module
+
+#### starting built-in functions for numbers ####
 # max => get height number
 # min => get small number
 # round => Rounding to the nearest number
@@ -7,20 +9,47 @@
 # sqrt => Quadratic Islands || الجزر التربيعي
 # pow => for example pow(10, 4) => 10 * 10 * 10 * 10
 
-print("#### starting built-in functions for numbers ####")
-print(max(10, 99))
-print(min(10, 99))
-print(pow(10, 4))
-print(round(10.6))
 
-from math import floor
-print(floor(10.6))
+#### starting built-in functions for strings ####
+# lower
+# upper
+# islower
+# isupper
+# len
+# index
+# replace
 
-from math import ceil
-print(ceil(10.2))
 
-from math import sqrt
-print(sqrt(100))
-
+#----------------------------------------------------------------
+print("#### starting built-in functions for numbers ####")     #|
+print(max(10, 99))                                             #|
+print(min(10, 99))                                             #|
+print(pow(10, 4))                                              #|
+print(round(10.6))                                             #|
+                                                               #|
+from math import floor                                         #|
+print(floor(10.6))                                             #|
+                                                               #|
+from math import ceil                                          #|
+print(ceil(10.2))                                              #|
+                                                               #|
+from math import sqrt                                          #|
+print(sqrt(100))                                               #|
+#----------------------------------------------------------------
 
 print("#### starting built-in functions for strings ####")
+print("#################################################")
+name = 'ashraf'
+namecapital = 'MOHAMED'
+stady = 'in_delta'
+
+print(name.upper())
+print(namecapital.lower())
+print(name.islower())
+print(name.isupper())
+print(namecapital.islower())
+print(namecapital.isupper())
+
+print(len(name))
+print(name.index('a'))
+print(name.replace('ashraf', 'ASHRAF'))
