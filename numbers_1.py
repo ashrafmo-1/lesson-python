33c0b6c8913906d7dfaf23412fad107c0f5f5e76
Ashraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2023-10-15 07:48:22 +0300

    Add files via upload
    nubmers and built-in functions from the numbers

---
+++ numbers_1.py
@@ -0,0 +1,27 @@
+from math import *
+
+# numbers data type
+
+print('---------- first examble ----------')
+
+numOne = 20
+numTwo = 21
+
+print(numTwo - numOne + 11 / 2 * 1)
+
+print('---------- second examble ----------')
+
+# some built-in functions from the numbers
+number = 1000
+absoluteNumber = -999
+print(str(number) + ' this is number of examble')
+# absolute القيمه بالموجوب
+print(abs(absoluteNumber))
+# pow الاوس 
+print(pow(4, 2))
+print(min(4, 2))
+print(max(4, 2))
+print(round(4.6)) # return number if under .5
+print(floor(4.9)) # return smallest number
+print(ceil(4.1)) # return biggest number
+print(sqrt(9)) # الجزر التربيعي
