7887ca6b16682f399a2287e42c080320d4cc21ed
Ashraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2023-10-15 07:01:12 +0300

    Add files via upload
    learn string and some built-in fundtion

---
+++ strings.py
@@ -0,0 +1,20 @@
+# start learning strings and some built-in functions
+line = '----------'
+print(line + ' examble one ' + line)
+name = 'ASHRAF'
+print('python \n string ' + name.lower())
+print('python\tstring')
+print('python\"string')
+print(name.islower())
+print(name.isupper ())
+print(len(name))
+print(name[0])
+print(name.index('S'))
+print(name.replace('ASHRAF', 'ashraf'))
+print(name)
+print(line + ' examble two ' + line)
+name = 'mohamed'
+print('python string' + name.upper())
+print(name.islower())
+print(name.isupper ())
+print(name[-1])
