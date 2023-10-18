75806b2819abe51c6838ae5d96b70497cfa519ee
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-10-18 19:07:25 +0200

    add new aclc sum only and qustions to reply the solution

---
+++ make calc/calc.py
@@ -0,0 +1,8 @@
+firstNum = input('type first number:> ')
+secondNum = input('type second number:> ')
+
+# int is function convert string to integer number and not supported 0.1
+# int is function convert string to integer number and supported 0.1
+result = float(firstNum) + float(secondNum)
+
+print(result)
---
+++ questions and reply selution/qustionANdreplySolution.py
@@ -0,0 +1,7 @@
+loveColor = input('waths your colors love:> ')
+loveStudy = input('waths your Study love:> ')
+lovepepol = input('waths your pepol love:> ')
+
+print('your colors love is ' + loveColor)
+print('your colors Study is ' + loveStudy)
+print('your colors pepol is my ' + lovepepol)
