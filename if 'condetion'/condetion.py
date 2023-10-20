8c7998b04f47eab419d024b15293096aac3a83e0
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-10-20 23:09:33 +0200

    add condetion

---
+++ if 'condetion'/condetion.py
@@ -0,0 +1,21 @@
+hangry = False
+
+if hangry:
+    print('i am hangry')
+else:
+    print('i am not hangry')
+
+
+# comming soon ( 'or', 'and' )
+
+number = 4
+if number == 0:
+    print('i am zero')
+elif number == 1:
+    print('i am one')
+elif number == 2:
+    print('i am two')
+elif number == 3:
+    print('i am three')
+else:
+    print('nan')
