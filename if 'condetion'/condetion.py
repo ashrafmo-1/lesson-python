25735dfe2f204d7c22019b6335c2fb09bc01c91b
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-10-24 17:17:18 +0200

    edit some line i if condition

--- if 'condetion'/condetion.py
+++ if 'condetion'/condetion.py
@@ -1,21 +1,26 @@
 hangry = False
 
-if hangry:
-    print('i am hangry')
+# if hangry:
+#     print('i am hangry')
+# else:
+#     print('i am not hangry')
+
+# comming soon ( 'or', 'and', 'not )
+
+number = 1
+# if number == 0 and hangry:
+#     print('i am zero' + ' and i am hangry')
+# elif number == 1:
+#     print('i am one')
+# elif number == 2:
+#     print('i am two')
+# elif number == 3:
+#     print('i am three')
+# else:
+#     print('nan')
+
+
+if number == 0 and hangry:
+    print('i am zero' + ' and i am hangry')
 else:
-    print('i am not hangry')
-
-
-# comming soon ( 'or', 'and' )
-
-number = 4
-if number == 0:
-    print('i am zero')
-elif number == 1:
-    print('i am one')
-elif number == 2:
-    print('i am two')
-elif number == 3:
-    print('i am three')
-else:
-    print('nan')
+    print('i am not zero' +' and i am not hangry')
