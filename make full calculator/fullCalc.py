6019c8eb6fcbf536f35aad1b6859e698f130b0c4
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-10-26 06:28:13 +0200

    add while loops

--- make full calculator/fullCalc.py
+++ make full calculator/fullCalc.py
@@ -1,1 +1,15 @@
 # full calculation
+
+numberOne = float(input('inter number one:> '))
+operator = input('inter operator')
+numberTwo = float(input('inter number two:> '))
+
+
+if operator == '+':
+    print(numberOne + numberTwo)
+elif operator == '-':
+    print(numberOne - numberTwo)
+elif operator == '*':
+    print(numberOne * numberTwo)
+elif operator == '/':
+    print(numberOne / numberTwo)
---
+++ while loop/while.py
@@ -0,0 +1,7 @@
+array = 1
+
+while array <= 10:
+    print(array)
+    array += 1
+else:
+    print('array has end ')
