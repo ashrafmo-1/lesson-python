05351709b3d9d3d1fd57e803acfe7d6484e38fba
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-10-31 18:04:33 +0200

    add new file delta sudy

--- learn python from delta/test.py
+++ learn python from delta/test.py
@@ -1,4 +1,10 @@
-10 # full calculation
+# print('''ashraf
+#     mohamed''')
+# print('ashraf\nmohamed')
+
+
+# inputType = input('type your name:> ')
+# print('hello ', inputType)
 
 numberOne = float(input('inter number one:> '))
 operator = input('inter operator')
@@ -13,4 +19,3 @@ elif operator == '*':
     print(numberOne * numberTwo)
 elif operator == '/':
     print(numberOne / numberTwo)
-
