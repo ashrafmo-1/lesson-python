37d7d02097a2bad306cea3ba8a9c01f4c5747c09
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-11-02 11:43:02 +0200

    add 2DList nestes loop

---
+++ 2D Lists & Nested Loops/2D Lists & Nested Loops.py
@@ -0,0 +1,20 @@
+# 2D lists
+list= [
+        [[1,2,3,4],1 , 2 , 3 , 4 , 5 , 6 , 7 , 8],
+        [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8],
+        [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+]
+
+print(list[0][0][0])
+
+
+# Nested Loops
+'''
+rtwggggggggggggggggggggg
+'''
+
+for row in list:
+    print(row)
+
+    for num in row:
+        print(num * 2 )
