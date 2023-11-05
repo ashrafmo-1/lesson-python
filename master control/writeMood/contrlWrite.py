d133e5f5d4a58babce344dc58343865f688a8935
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-11-05 20:03:46 +0200

    errors and make file html

--- master control/writeMood/contrlWrite.py
+++ master control/writeMood/contrlWrite.py
@@ -1,7 +1,7 @@
 # [w ===> write] ====> remove all lines from the file and new typing
 # [a ===> append] ===> add a new line at the end of the file
-writeControls = open('D:\learn python\lessons-python/strings.py', 'a') #open the file
+writeControls = open('D:\learn python\lessons-python/index.html', 'a') #open the file
 arr = ['\nhello', 'world', '!']
-print(writeControls.writelines(arr))
+print(writeControls.writelines("<body><h2>make file by by python</h2></body>"))
 print(writeControls.readable())
 writeControls.close()
