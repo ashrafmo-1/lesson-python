97d8dd97952c8b7df2c19a3109592e85d0d253c2
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-11-11 22:09:04 +0200

    add more methods to list control

--- learn python from delta/listControl.py
+++ learn python from delta/listControl.py
@@ -2,24 +2,26 @@ my_list = [1, 2, 3, 4, 5]
 
 print(my_list[0])
 print(my_list[1:4])
+print(my_list[2: ])
+print(my_list[3: -1])
 
-# my_list.append(6)
-# print(my_list)
+my_list.append(6)
+print(my_list)
 
-# my_list.insert(0, 7)
-# print(my_list)
+length = len(my_list)
+index = my_list.index(4) # ==> get the index of the first element
 
-# my_list.remove(3)  # Remove the first occurrence of 3
-# print(my_list)
+my_list.insert(0, 7) # replace the zero number with the seven number
+print(my_list)
 
-# popped_element = my_list.pop(1)  # Removes and returns the last element
-# print(popped_element)
+my_list.remove(3)  # Remove the first occurrence of 3
+print(my_list)
 
-# length = len(my_list)
-# index = my_list.index(4)
+popped_element = my_list.pop(1)  # Removes and returns the last element
+print(popped_element)
 
-# my_list.sort()
+my_list.sort() # ==> sorted list
 
-repeated_list = my_list * 3
+repeated_list = my_list * 3  # repeated list three times
 
-print(repeated_list) # repeated list three times
+print(repeated_list)
