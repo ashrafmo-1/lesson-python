6aa14c2f8019195931a46b99787fc7e83c3fb995
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-12-02 05:46:09 +0200

    learn delta file

---
+++ learn python from delta/list.py
@@ -0,0 +1,20 @@
+arr = [1,2,3,4,5,6]
+
+arr.append(7)
+arr.extend([8,9])
+
+del arr[0]
+
+print(arr)
+
+
+# tuple
+x = (1,3,4,5,5,6,6)
+
+print(x[0])
+
+# start set
+# can user add and remove values 
+# cant programmer replace any values
+# the set and order and un indexed
+# 
