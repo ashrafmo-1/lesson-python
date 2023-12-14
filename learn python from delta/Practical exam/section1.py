833250a467be058754aed48a405b051d3da40729
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-12-14 13:35:18 +0200

    data type

---
+++ learn python from delta/Practical exam/section1.py
@@ -0,0 +1,19 @@
+# print('')
+# data type => انواع البيانات
+# string => النص بيتكتب جوه '' او "" .
+# number => الارقام
+print(10)
+print(1.5)
+# type => بنشوف بيها نوع البيانات المدخل
+print(type(10)) #int => integar
+print(type(1.5)) # float
+print('az')
+print("as")
+print(type('az')) # str => string
+# boolean => (True, False)
+print(True)
+print(False)
+print(type(False)) # bool => boolean
+# number + data type
+# string + data type
+# boolean + data type
