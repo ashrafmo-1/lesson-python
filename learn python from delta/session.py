40c8a6778839a0d7efe3f4d7496cc88c525fa22e
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-12-19 00:38:34 +0200

    new lessons

--- learn python from delta/session.py
+++ learn python from delta/session.py
@@ -1,45 +1,37 @@
-# print('''sheno
-# ''')
-# print("""shenoo""")
+list=['shin' ,10 ,10.5 ]
+print(list)
+print(list[0: 2])
+soo = list[0:2]
+print(soo)
 
-# variables > المتغيرات
-# var name  => value variable
-# اسم المغير مينعش يبدا بارقام
-# camel case => firstName
-# FirstName = 'sheno'
-# live = '3aysh' # reuseable variable
+lst = [1,2,3,4,5,6,7,8]
+lst2 = ['rania', 'elshenawy', 'ashf']
 
-# print(firstName)
-# print(type(firstName))
-# print(live)
-# print(type(live))
+print(lst)
+lst.append(100) # add
+print(lst)
+lst.remove(2) # remove from any location
+print(lst)
 
-# operators => العمليات الحسابيه
-# + - * / % **
-print(10 + 1)
-sum = 10 + 1
-print(sum)
+x = lst2 + lst
+print(x)
+x.reverse()
+print(x)
+x.clear()
+print(x)
+
+# tuples
+t = (1, 2, 3)
+
+print(t)
+
+set = {11,11,22,22, 'a', 'a', 'a', 'a', 'a', 'a', 'a','b', 'b'}
+print(set) # un orderd
+set.add('d3bs')
+print(set) # un orderd
+set.add('d3bs3')
+print(set) # un orderd
+set.add('shenoo')
+print(set) # un orderd
 
 
-print(10 - 5)
-print(10 / 2) # / => سلاش واحده بتقسم وترن رقم float
-print(10 // 2) # // => دبل سلاش بترن رقم integer
-
-print(10 * 2) # بتضرب
-print(10 ** 2) # الاوس
-
-print(10 % 2) # باقي القسمه
-
-str = 'ashraf mohamed'
-
-print(str)
-print(str[0 : 6]) # not including end
-
-name = str[0 : 6]
-print('var =>', name)
-
-print = (10) # tuple
-
-# list tuple Dictionary fuction if
-# numpy pandas
-# + 100 نظري
