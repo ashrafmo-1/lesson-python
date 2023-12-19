6499568af317f164fbcc74b096b9f93899a25ae0
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-12-19 13:21:44 +0200

    course

---
+++ learn python  to users/master.py
@@ -0,0 +1,22 @@
+# why python learning
+# why downloading python on my divise
+# tools working on python
+# basecs python
+
+# print
+# variables
+# datatypes
+# functions
+# loops
+# methods
+# regular expressions
+# destructuring
+# built-in functions
+# libraries
+# tuples
+# dectinarys
+# time to learning python
+# before learn python
+# can programint learn net work and can be lern stpeSecurty
+# commitnt
+# problem silving
